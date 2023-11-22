35978c84ed0cba2a24f8b3d14d86e567aeff576e
Tanzeel <tanzeel498@gmail.com>
2023-11-23 01:03:22 +0500

    add responsive product page and fix modal responsiveness

--- src/features/product/ProductTitle.jsx
+++ src/features/product/ProductTitle.jsx
@@ -2,7 +2,7 @@ import { formatCurrency } from "../../utils/helpers";
 
 function ProductTitle({ title, subTitle, fullPrice, currentPrice }) {
   return (
-    <div className="my-2">
+    <div className="my-5 px-6 tablet:my-2 tablet:px-0">
       <h2>{title}</h2>
       <p>{subTitle}</p>
       <p className="my-3 font-medium">{formatCurrency(currentPrice)}</p>
