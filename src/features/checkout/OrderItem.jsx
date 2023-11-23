6030938c5abae15e2a9ad87148f7995fa3a1501e
Tanzeel <tanzeel498@gmail.com>
2023-11-24 00:09:25 +0500

    make checkout page responsive

--- src/features/checkout/OrderItem.jsx
+++ src/features/checkout/OrderItem.jsx
@@ -3,12 +3,12 @@ import { data } from "../cart/bagData";
 
 function OrderItem() {
   return (
-    <div className="flex gap-5">
+    <div className="flex w-full gap-5 mobile:w-2/5 tablet:w-full">
       <div className="shrink-0">
         <img className="w-16" src={data[0].images.squarishURL} alt="" />
       </div>
       <div className="flex flex-col flex-wrap text-stone-500">
-        <span className="text-stone-900">{data[0].title}</span>
+        <span className="font-semibold text-stone-900">{data[0].title}</span>
         <span>Style #: {"DV3278-102"}</span>
         <span>Size: {"9.5"}</span>
         <span>Color: {data[0].colorDescription}</span>
