bb7abd5b6ae9b250374ed063332d7fb2a88dd3d7
Tanzeel <tanzeel498@gmail.com>
2024-03-08 00:27:43 +0500

    remove scrollbar from Product thumbs and fix thumbImage doesn't exists issue

--- src/features/product/CarouselProduct.jsx
+++ src/features/product/CarouselProduct.jsx
@@ -11,14 +11,6 @@ function CarouselProduct() {
   const { images } = product?.colors?.at(0) || {};
   const imagesLength = images?.length || 0;
 
-  // useEffect(
-  //   function () {
-  //     // this useEffect will check if image exists at  thumbActive
-  //     if (thumbActive > imagesLength) setThumbActive(0);
-  //   },
-  //   [thumbActive, imagesLength],
-  // );
-
   function handleNext() {
     setThumbActive((active) => {
       if (active === imagesLength - 1) return 0;
@@ -33,22 +25,27 @@ function CarouselProduct() {
     });
   }
 
+  // this will check if image exists at thumbActive
+  if (!isLoading && thumbActive > imagesLength - 1) {
+    setThumbActive(0);
+    return;
+  }
+
   return isLoading ? (
     <CarouselProductBlank />
   ) : (
     <div className="top-16 flex h-[700px] gap-4 tablet:sticky tablet:w-2/5 tablet:justify-end">
       <div className="thumbsContainer hidden w-16 flex-shrink-0 flex-col gap-2 overflow-y-auto tablet:flex">
         {images?.map((img, index) => (
-          <div key={index} className="thumb">
-            <img
-              src={img.srcThumbnail}
-              alt={img.alt}
-              className={`h-16 w-16 rounded-md border-stone-900 object-cover duration-100 ${
-                thumbActive === index ? "border-[1px]" : ""
-              }`}
-              onMouseEnter={() => setThumbActive(index)}
-            />
-          </div>
+          <img
+            key={index}
+            src={img.srcThumbnail}
+            alt={img.alt}
+            className={`h-16 w-16 rounded-md border-stone-900 object-cover duration-100 ${
+              thumbActive === index ? "border-[1px]" : ""
+            }`}
+            onMouseEnter={() => setThumbActive(index)}
+          />
         ))}
       </div>
 
